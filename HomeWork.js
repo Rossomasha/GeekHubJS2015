0ef766c29b25871f29280efdc0e34a9519a95f66
Stanislav Feshchenko <feshchenkostanislav@gmil.com>
2015-10-22 16:18:15 +0300

    Home Work #1.3

--- HomeWork.js
+++ HomeWork.js
@@ -6,28 +6,24 @@ function chit(){
         while(isFree('west')){
         west();
         step=step+'L';//Дописывает в переменную в какую сторону было совершено движение.
-        map();
         };
     }
     else if (isFree('east')&&(step[step.length-1]!='L')){
         while(isFree('east')&&returnAnser!="end"){//Крутить цикл пока east() не вернет "end"
         returnAnser=east();
         step=step+'R';
-        map();
         };
     }
     else if (isFree('south')&&(step[step.length-1]!='T')){
         while(isFree('south')){
         south();
         step=step+'B';
-        map();
         };
     }
     else if (isFree('north')&&(step[step.length-1]!='B')){
         while(isFree('north')){
         north()
         step=step+'T';
-        map();
         };
     }
     else{
@@ -35,124 +31,44 @@ function chit(){
         step=step+"trable-T";//В случае тупика сделать шаг в верх и отметить направление шага в истории как trable-T
     }
 }
-
-while (returnAnser!="end"){//Крутить цикл пока east() не вернет "end"
+while (returnAnser!="end"){//Крутим функцию в цикле пока east() не вернет "end"
     chit();
 };
 //***********************************************
 
 //Обычное прохождение лаберинта.
 
-// Обёртка функций для движения чтобы иметь возможность задавать им количество шагов.
-
-function up(a){
-    for(var i=1; i<a ;i++){
-    north();}
-    return north();
-}
-function right(a){
-    for(var i=1; i<a ;i++){
-    east();}
-    return east();
-}
-function bot(a){
-    for(var i=1; i<a ;i++){
-    south();}
-    return south();
-}
-function left(a){
-    for(var i=1; i<a ;i++){
-    west();}
-    return west();
-}
-
-//Функции для движения по Г-образной направляющей.
-
-function rightBot(){
-    if (isFree('east')){
-        while (isFree('east')&&right());
-        rightBot();
-    }
-    else if (isFree('south')){
-        while (isFree('south')&&bot());
-        rightBot();
-    } 
-}
-function leftBot(){
-    if (isFree('west')){
-        while (isFree('west')&&left());
-        leftBot();
-    }
-    else if (isFree('south')){
-        while (isFree('south')&&bot());
-        leftBot();
-    } 
-}
-function leftUp(){
-    if (isFree('west')){
-        while (isFree('west')&&left());
-        leftUp();
-    }
-    else if (isFree('north')){
-        while (isFree('north')&&up());
-        leftUp();
-    } 
-}
-function rightUp(){
-    if (isFree('east')){
-        while (isFree('east')&&right());
-        rightUp();
-    }
-    else if (isFree('north')){
-        while (isFree('north')&&up());
-        rightUp();
-    } 
-}
-
 // 1 level
-
-while (isFree('north')&&up()); // Метод движения до упора в стену.
-console.log("Уровень 1 пройден");
-
+while (isFree('north')&&north()); // Метод движения до упора в стену.
 // 2 level
-
-while (isFree('east')&&right());
-console.log("Уровень 2 пройден");
-
+while (isFree('east')&&east());
 // 3-4 level
-
-rightBot();
-console.log("Уровень 3 и 4 пройден");
-
+while (isFree('east')&&east()||isFree('south')&&south());
 // 5 level
-
-leftBot();
-leftUp();
-leftBot();
-while (isFree('east')&&right());
-console.log("Уровень 5 пройден");
-
+while (isFree('west')&&west()||isFree('south')&&south());
+while (isFree('west')&&west()||isFree('north')&&north());
+while (isFree('west')&&west()||isFree('south')&&south());    
+while (isFree('east')&&east());
 // 6 level
-
-rightBot();
-rightUp();
-rightBot();
-up();
-leftBot();
-while (isFree('east')&&right());
-console.log("Уровень 6 пройден");
-
+while (isFree('east')&&east()||isFree('south')&&south());
+while (isFree('east')&&east()||isFree('north')&&north());
+while (isFree('east')&&east()||isFree('south')&&south());
+north();
+while (isFree('west')&&west()||isFree('south')&&south());  
+while (isFree('east')&&east());
 // 7 level
-
-bot(11); //Метод движения с заданием количества шагов.
-right(4);
-while (isFree('north')&&up());
-while (isFree('east')&&right());
-rightBot();
-left();
-while (isFree('south')&&bot());
-while (right()!="end"){
-    while (isFree('east')&&right());
-    while (isFree('south')&&bot());
-};
-console.log("Уровень 7 пройден");
+var e;
+while (isFree('south')&&south());
+while (isFree('east')&&east());  
+while (isFree('north')&&north());
+while (isFree('east')&&east());
+while (isFree('east')&&east()||isFree('south')&&south());
+while (isFree('west')&&west());
+while (isFree('south')&&south());
+while (isFree('west')&&west());
+while (e!="end"){
+    while (isFree('south')&&south());
+    while (isFree('east')&&e!="end"){
+        e=east();
+    }
+}
